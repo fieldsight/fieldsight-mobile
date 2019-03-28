a2554c1bdf8c385215c21b9b547ab14240230ba2
yubaraj poudel <hereyubaraj@gmail.com>
2019-03-28 15:28:09 +0545

    test script updated

--- fs.sh
+++ fs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+BRANCH_NAME = "master"
+
 installApp() {
 # run adb
 [! -f "gradlew"] && echo "gradlew file not exists" && exit
@@ -11,30 +13,39 @@ runApp() {
    adb install ./collect_app/build/outputs/apk/debug/*.apk
 }
 
+askToTestRelease() {
+    read -p "Do you want to test realease (y/n)?" choice
+    case "$choice" in
+      y|Y ) echo "pulling master" ;;
+      n|N ) exit;;
+      * ) echo "invalid" && exit;;
+    esac
+}
+
 #check the branch name is provided in the command or not
 if [ "$#" -ne 1 ]; then
     echo "Need to provide pullrequest(branch) name";
     echo "https://github.com/fieldsight/fieldsight-mobile/pulls";
+    # confirm whether want to test default master
+    askToTestRelease
+else BRANCH_NAME = $1
 fi
-# confirm whether want to test default master
-read -p "Do you want to test realease (y/n)?" choice
-case "$choice" in
-  y|Y ) set -- $(1:-master);;
-  n|N ) exit;;
-  * ) echo "invalid" && exit;;
-esac
-echo "$1";
+
+echo "$BRANCH_NAME";
+
 # get the current branch name
 BRANCH=$(git branch)
+
 # check the current branch is master or not
 [[ $BRANCH != "master" ]] && git checkout master
+
 #pull the branch from origin
-git pull origin "$1"
+git pull origin "$BRANCH_NAME"
 
 if [ $? -eq 0 ]; then
     echo "$1 is fetched successfully";
     #checkout to the branch
-    git checkout "$1"
+    git checkout "$BRANCH_NAME"
     runApp
 else
     echo "Failed to pull, please check branch name"
