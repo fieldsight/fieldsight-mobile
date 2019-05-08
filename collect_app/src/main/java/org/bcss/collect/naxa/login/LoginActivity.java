ef18f5db662e05dcb2eb42e43da56849ae23f5e8
samirdangal <samirdangal@gmail.com>
2019-05-08 16:28:11 +0545

    [#136] Catch gmail Sign In failed error

--- collect_app/src/main/java/org/bcss/collect/naxa/login/LoginActivity.java
+++ collect_app/src/main/java/org/bcss/collect/naxa/login/LoginActivity.java
@@ -133,6 +133,11 @@ public class LoginActivity extends BaseLoginActivity implements LoginView {
 
     }
 
+    @Override
+    public void gmailLoginFailed(String errorMsg) {
+        showError(errorMsg);
+    }
+
     /**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
