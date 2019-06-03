364a140cbb09750cb232a8e2ebab209d30bdd13d
Nishon Tandukar <nishon.tan@gmail.com>
2019-06-03 16:49:19 +0545

    [#196] loads site documents and caches them in site doc activity

--- collect_app/src/main/java/org/bcss/collect/naxa/sitedocuments/SiteDocumentsListActivity.java
+++ collect_app/src/main/java/org/bcss/collect/naxa/sitedocuments/SiteDocumentsListActivity.java
@@ -15,10 +15,26 @@ import org.bcss.collect.android.R;
 import org.bcss.collect.naxa.common.GridItemDecoration;
 import org.bcss.collect.naxa.common.RecyclerViewEmptySupport;
 import org.bcss.collect.naxa.login.model.Site;
+import org.bcss.collect.naxa.network.APIEndpoint;
+import org.bcss.collect.naxa.network.ServiceGenerator;
+import org.bcss.collect.naxa.v3.network.ApiV3Interface;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.odk.collect.android.activities.CollectAbstractActivity;
+
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.Single;
+import io.reactivex.SingleSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
+import timber.log.Timber;
 
 import static org.bcss.collect.naxa.common.Constant.EXTRA_OBJECT;
 
@@ -49,8 +65,46 @@ public class SiteDocumentsListActivity extends CollectAbstractActivity implement
 
         loadedSite = getIntent().getExtras().getParcelable(EXTRA_OBJECT);
 
+
         setupToolbar();
         setupRecyclerView();
+        getDataFromRemote();
+    }
+
+
+    void getDataFromRemote() {
+        HashMap<String, String> params = new HashMap<>();
+        params.put(APIEndpoint.PARAMS.SITE_ID, loadedSite.getId());
+
+        ServiceGenerator.createCacheService(ApiV3Interface.class)
+                .getSiteDocuments(params)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Function<ResponseBody, SingleSource<List<String>>>() {
+                    @Override
+                    public SingleSource<List<String>> apply(ResponseBody responseBody) throws Exception {
+                        JSONObject jsonObject = new JSONObject(responseBody.string());
+                        JSONArray result = jsonObject.getJSONArray("blueprints");
+                        return io.reactivex.Observable.range(0, result.length())
+                                .map(new Function<Integer, String>() {
+                                    @Override
+                                    public String apply(Integer index) throws Exception {
+                                        return result.getString(index);
+                                    }
+                                }).toList();
+                    }
+                })
+                .subscribe(new DisposableSingleObserver<List<String>>() {
+                    @Override
+                    public void onSuccess(List<String> urls) {
+                        adapter.addAll(urls);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e("Failed to add documents to list due to %s", e.getMessage());
+                    }
+                });
     }
 
     private void setupToolbar() {
@@ -69,9 +123,7 @@ public class SiteDocumentsListActivity extends CollectAbstractActivity implement
         rvSiteDocuments.setAdapter(adapter);
         rvSiteDocuments.setEmptyView(emptyLayout, getString(R.string.empty_message_site_documents), null);
         rvSiteDocuments.addItemDecoration(new GridItemDecoration(16, 2));
-        if(loadedSite.getSiteDocuments() != null){
-            adapter.addAll(loadedSite.getSiteDocuments());
-        }
+
 
     }
 
