93912691dddeb3e76fe54cf13222713fe2b2a4ca
yubaraj poudel <hereyubaraj@gmail.com>
2019-06-20 14:07:03 +0545

    [#233]-ui report added

---
+++ collect_app/src/main/java/org/bcss/collect/naxa/report/ReportActivity.java
@@ -0,0 +1,15 @@
+package org.bcss.collect.naxa.report;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import org.odk.collect.android.activities.CollectAbstractActivity;
+
+public class ReportActivity extends CollectAbstractActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
