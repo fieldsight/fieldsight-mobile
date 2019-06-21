58843e0dc20bfb4b98426fd8890fb300be919475
yubaraj poudel <hereyubaraj@gmail.com>
2019-06-21 11:31:27 +0545

    notificationview model copied

--- collect_app/src/main/java/org/bcss/collect/naxa/notificationslist/NotificationListViewModel.java
+++ collect_app/src/main/java/org/bcss/collect/naxa/notificationslist/NotificationListViewModel.java
@@ -3,17 +3,32 @@ package org.bcss.collect.naxa.notificationslist;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.util.Log;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
 import org.bcss.collect.naxa.data.FieldSightNotification;
+import org.bcss.collect.naxa.data.source.local.FieldSightNotificationLocalSource;
+import org.bcss.collect.naxa.login.model.Project;
+import org.bcss.collect.naxa.login.model.SiteMetaAttribute;
+import org.bcss.collect.naxa.network.NetworkUtils;
+import org.bcss.collect.naxa.network.ServiceGenerator;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.lang.reflect.Type;
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
@@ -44,42 +59,7 @@ public class NotificationListViewModel extends ViewModel {
         return notificationRepository.getAll(false);
     }
 
-    void pullDataFromServer(String epochTime) {
-        NotificationRemoteSource.getInstance().getNotifications(0)
-                .toObservable()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .flatMap((Function<ResponseBody, ObservableSource<JSONObject>>) responseBody -> {
-                    Timber.i("NotificationListViewModel value = %s", responseBody.string());
-                    JSONArray jsonArray = new JSONObject(responseBody.string()).optJSONArray("notifications");
-                    Timber.i("NotificationListViewModel, val length:: %d", jsonArray.length());
-                    return Observable.range(0, jsonArray.length())
-                            .map(jsonArray::getJSONObject);
-                })
-                .map(new Function<JSONObject, FieldSightNotification>() {
-                    @Override
-                    public FieldSightNotification apply(JSONObject json) throws Exception {
-                        FieldSightNotification fieldSightNotification = new FieldSightNotification();
-                        fieldSightNotification.setNotifiedDate(json.optString("data"));
-                        fieldSightNotification.setProjectId(json.optJSONObject("message").optJSONObject("project").optString("id"));
-                        fieldSightNotification.setProjectName(json.optJSONObject("message").optJSONObject("project").optString("name"));
-                        fieldSightNotification.setNotificationType(json.optJSONObject("message").optString("notify_type"));
-                        fieldSightNotification.setSiteId(json.optJSONObject("message").optJSONObject("site").optString("id"));
-                        fieldSightNotification.setSiteName(json.optJSONObject("message").optJSONObject("site").optString("name"));
-                        return fieldSightNotification;
-                    }
-                })
-                .toList()
-                .subscribe(new DisposableSingleObserver<List<FieldSightNotification>>() {
-                    @Override
-                    public void onSuccess(List<FieldSightNotification> list) {
-                        notificationRepository.save((ArrayList<FieldSightNotification>) list);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Timber.e(e);
-                    }
-                });
+    void saveData(List<FieldSightNotification> fieldSightNotifications) {
+        notificationRepository.save((ArrayList<FieldSightNotification>) fieldSightNotifications);
     }
 }
