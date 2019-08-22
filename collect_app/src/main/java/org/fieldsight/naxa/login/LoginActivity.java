ec449f05ccb9dde1285c980a5ef67aed7728583f
Nishon Tandukar <nishon.tan@gmail.com>
2019-08-22 12:53:17 +0545

    Does not show dialog is login activity is closing (#357)

--- collect_app/src/main/java/org/fieldsight/naxa/login/LoginActivity.java
+++ collect_app/src/main/java/org/fieldsight/naxa/login/LoginActivity.java
@@ -218,7 +218,9 @@ public class LoginActivity extends BaseLoginActivity implements LoginView {
     }
 
     private void showErrorDialog(String msg) {
-
+        if(isFinishing()){
+            return;
+        }
         Dialog dialog = DialogFactory.createActionDialog(LoginActivity.this, "Login Failed", msg)
                 .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
                     @Override
@@ -229,7 +231,7 @@ public class LoginActivity extends BaseLoginActivity implements LoginView {
                 })
                 .setNegativeButton(R.string.dialog_action_dismiss, null)
                 .create();
-        new Handler().postDelayed(dialog::show, 500);
+       dialog.show();
     }
 
     /**
