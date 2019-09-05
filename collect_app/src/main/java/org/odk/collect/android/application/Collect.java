4e1150df242e4afd3e46da01956519815a8222cc
samirdangal <samirdangal@gmail.com>
2019-09-05 13:33:31 +0545

    [#246] hide API Compatibility warning dialog (Settings page)

--- collect_app/src/main/java/org/odk/collect/android/application/Collect.java
+++ collect_app/src/main/java/org/odk/collect/android/application/Collect.java
@@ -74,6 +74,9 @@ import org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -286,6 +289,9 @@ public class Collect extends Application {
         if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this);
         }
+
+        hideAndroidApiCompatibilityWarningDialog();
+
         setupFirebaseRemoteConfig();
         OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
 
@@ -509,6 +515,28 @@ public class Collect extends Application {
                         }
                     }
                 });
+    }
+
+
+    private void hideAndroidApiCompatibilityWarningDialog(){
+        try {
+            Class aClass = Class.forName("android.content.pm.PackageParser$Package");
+            Constructor declaredConstructor = aClass.getDeclaredConstructor(String.class);
+            declaredConstructor.setAccessible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            Class cls = Class.forName("android.app.ActivityThread");
+            Method declaredMethod = cls.getDeclaredMethod("currentActivityThread");
+            declaredMethod.setAccessible(true);
+            Object activityThread = declaredMethod.invoke(null);
+            Field mHiddenApiWarningShown = cls.getDeclaredField("mHiddenApiWarningShown");
+            mHiddenApiWarningShown.setAccessible(true);
+            mHiddenApiWarningShown.setBoolean(activityThread, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
