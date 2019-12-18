fee7306900fbabbb735bd24ffec1dd19a64f5e0f
yubaraj poudel <yubarajpoudel708@gmail.com>
2019-12-18 11:51:19 +0545

    form list row ui changed

--- collect_app/src/main/java/org/fieldsight/naxa/forms/ui/FieldSightFormVH.java
+++ collect_app/src/main/java/org/fieldsight/naxa/forms/ui/FieldSightFormVH.java
@@ -2,6 +2,7 @@ package org.fieldsight.naxa.forms.ui;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,29 +13,31 @@ import org.fieldsight.naxa.forms.data.local.FieldsightFormDetailsv3;
 import org.odk.collect.android.logic.FormDetails;
 
 public class FieldSightFormVH extends RecyclerView.ViewHolder {
-    private final TextView tvTitle, tvSubtitle, tvIconText;
-    private final Button btnViewSubmission, btnViewEduMaterial;
+//    private final TextView tvTitle, tvSubtitle, tvIconText;
+//    private final Button btnViewSubmission, btnViewEduMaterial;
+    TextView tvFormName, tvLastSubmittedDate;
+    ImageView ivEducationMaterials;
 
     protected FieldSightFormVH(@NonNull View itemView) {
         super(itemView);
-        tvTitle = itemView.findViewById(R.id.tv_form_primary);
-        tvSubtitle = itemView.findViewById(R.id.tv_form_secondary);
-        tvIconText = itemView.findViewById(R.id.form_icon_text);
-        btnViewEduMaterial = itemView.findViewById(R.id.btn_form_edu);
-        btnViewSubmission = itemView.findViewById(R.id.btn_form_responses);
-
+//        tvTitle = itemView.findViewById(R.id.tv_form_primary);
+//        tvSubtitle = itemView.findViewById(R.id.tv_form_secondary);
+//        tvIconText = itemView.findViewById(R.id.form_icon_text);
+//        btnViewEduMaterial = itemView.findViewById(R.id.btn_form_edu);
+//        btnViewSubmission = itemView.findViewById(R.id.btn_form_responses);
+        tvFormName = itemView.findViewById(R.id.tv_form_name);
+        tvLastSubmittedDate = itemView.findViewById(R.id.tv_last_submitted);
+        ivEducationMaterials = itemView.findViewById(R.id.iv_education_materials);
     }
 
     public void bindView(FieldsightFormDetailsv3 form) {
         FormDetails formDetails = form.getFormDetails();
 
         try{
-            tvTitle.setText(formDetails.getFormName());
-            tvSubtitle.setText(formDetails.getFormName());
-            tvIconText.setText(formDetails.getFormName().substring(0, 1));
+            tvFormName.setText(formDetails.getFormName());
+            tvLastSubmittedDate.setText(formDetails.getFormName());
             itemView.setOnClickListener(view -> openForm(form));
-            btnViewEduMaterial.setOnClickListener(view -> openEducationalMaterial(form));
-            btnViewSubmission.setOnClickListener(view -> openPreviousSubmission(form));
+            ivEducationMaterials.setOnClickListener(view -> openEducationalMaterial(form));
         }catch (Exception e){
 
         }
