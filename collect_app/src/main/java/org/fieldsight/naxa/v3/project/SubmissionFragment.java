8fdbb251105fc0505684816f23317ef841bec52b
yubaraj poudel <yubarajpoudel708@gmail.com>
2020-01-03 11:50:05 +0545

    project submission ui added

--- collect_app/src/main/java/org/fieldsight/naxa/v3/project/SubmissionFragment.java
+++ collect_app/src/main/java/org/fieldsight/naxa/v3/project/SubmissionFragment.java
@@ -4,16 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.bcss.collect.android.R;
+
+import butterknife.ButterKnife;
+
 public class SubmissionFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_project_submission, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
