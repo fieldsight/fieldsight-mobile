3bb87c2578fdcdd94f66ee7f5c894ec6ec296a14
yubaraj poudel <yubarajpoudel708@gmail.com>
2020-01-24 11:44:04 +0545

    syncing adapter viewholder and view added

---
+++ collect_app/src/main/java/org/fieldsight/naxa/v3/adapter/ProjectSyncViewholder.java
@@ -0,0 +1,95 @@
+package org.fieldsight.naxa.v3.adapter;
+
+import android.content.res.Resources;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import org.bcss.collect.android.R;
+import org.fieldsight.naxa.login.model.Project;
+
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
+import static org.fieldsight.naxa.common.ViewUtils.loadImageWithFallback;
+
+
+public class ProjectSyncViewholder extends RecyclerView.ViewHolder {
+    @BindView(R.id.primary_text)
+    TextView primaryText;
+
+    @BindView(R.id.sub_text)
+    TextView textView;
+
+    @BindView(R.id.iv_sync)
+    ImageView ivSync;
+
+    @BindView(R.id.tv_synced_date)
+    TextView tvSyncedDate;
+
+    @BindView(R.id.iv_card_status)
+    AppCompatImageView imageView;
+
+    @BindView(R.id.iv_project_thumbnail)
+    ImageView ivThumbnail;
+
+    @BindView(R.id.tv_regions)
+    TextView tvRegions;
+
+    @BindView(R.id.tv_submissions)
+    TextView tvSubmissions;
+
+    @BindView(R.id.tv_sites)
+    TextView tvSites;
+
+    @BindView(R.id.tv_users)
+    TextView tvUsers;
+
+    @BindView(R.id.tv_label_regions)
+    TextView regionLabel;
+
+    @BindView(R.id.tv_label_submission)
+    TextView submissionLabel;
+
+    @BindView(R.id.tv_label_sites)
+    TextView sitesLabel;
+
+    @BindView(R.id.tv_label_users)
+    TextView userLabel;
+
+
+
+    public ProjectSyncViewholder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bindView(Project project, boolean allTrue) {
+        primaryText.setText(project.getName());
+        textView.setText(String.format("A project by %s", project.getOrganizationName()));
+        tvSyncedDate.setText(project.getStatusMessage());
+        Timber.i("project image = %s", project.getUrl());
+        Glide.with(itemView.getContext()).load(project.getUrl()).apply(RequestOptions.circleCropTransform()).into(ivThumbnail);
+        tvRegions.setText(String.format(Locale.ENGLISH, "%d", project.getTotalRegions()));
+        tvUsers.setText(String.format(Locale.ENGLISH, "%d", project.getTotalUsers()));
+        tvSubmissions.setText(String.format(Locale.ENGLISH, "%d", project.getTotalSubmissions()));
+        tvSites.setText(String.format(Locale.ENGLISH, "%d", project.getTotalSites()));
+
+        if (!TextUtils.isEmpty(project.getUrl())) {
+            loadImageWithFallback(itemView.getContext(), project.getUrl()).into(ivThumbnail);
+        } else {
+           ivThumbnail.setImageResource(R.drawable.fieldsight_logo);
+        }
+    }
+}
--- collect_app/src/main/java/org/fieldsight/naxa/v3/project/SyncingProjectAdapter.java
+++ collect_app/src/main/java/org/fieldsight/naxa/v3/project/SyncingProjectAdapter.java
@@ -9,11 +9,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.bcss.collect.android.R;
 import org.fieldsight.naxa.login.model.Project;
+import org.fieldsight.naxa.v3.adapter.ProjectSyncViewholder;
 import org.fieldsight.naxa.v3.adapter.ProjectViewHolder;
 
 import java.util.List;
 
-public class SyncingProjectAdapter extends RecyclerView.Adapter<ProjectViewHolder> {
+public class SyncingProjectAdapter extends RecyclerView.Adapter<ProjectSyncViewholder> {
     List<Project> projectList;
 
     public SyncingProjectAdapter(List<Project> projectList) {
@@ -22,13 +23,13 @@ public class SyncingProjectAdapter extends RecyclerView.Adapter<ProjectViewHolde
 
     @NonNull
     @Override
-    public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.project_expand, parent, false);
-        return new ProjectViewHolder(view);
+    public ProjectSyncViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.project_expand_sync, parent, false);
+        return new ProjectSyncViewholder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ProjectSyncViewholder holder, int position) {
         Project project = projectList.get(position);
         holder.bindView(project, false);
     }
