590b07c8c385a37d8749bbe1c28eaab1b42d04a2
Nishon Tandukar <nishon.tan@gmail.com>
2020-02-17 14:53:48 +0545

    remove email and username

--- collect_app/src/main/java/org/fieldsight/naxa/contact/ContactDetailsBottomSheetFragment.java
+++ collect_app/src/main/java/org/fieldsight/naxa/contact/ContactDetailsBottomSheetFragment.java
@@ -53,33 +53,33 @@ public class ContactDetailsBottomSheetFragment extends BottomSheetDialogFragment
                     .into(profilePicture);
         }
 
-        BindAndSetOrHide(fullname, R.id.tv_contactdetail_fullname, contactDetail.fullName);
+        bindAndSetOrHide(fullname, R.id.tv_contactdetail_fullname, contactDetail.fullName);
 
 
-        BindAndSetOrHide(role, R.id.tv_contactdetail_role, contactDetail.role);
+        bindAndSetOrHide(role, R.id.tv_contactdetail_role, contactDetail.role);
 
-        BindAndSetOrHide(address, R.id.tv_contactdetail_address, contactDetail.address);
+        bindAndSetOrHide(address, R.id.tv_contactdetail_address, contactDetail.address);
 
-        BindAndSetOrHide(gender, R.id.tv_contactdetail_gender, contactDetail.gender);
+        bindAndSetOrHide(gender, R.id.tv_contactdetail_gender, contactDetail.gender);
 
 
-        BindAndSetOrHide(skype, R.id.tv_contactdetail_skype, contactDetail.skype, R.id.iv_skype_icon);
+        bindAndSetOrHide(skype, R.id.tv_contactdetail_skype, contactDetail.skype, R.id.iv_skype_icon);
 
-        BindAndSetOrHide(twitter, R.id.tv_contactdetail_twitter, contactDetail.twitter, R.id.iv_twitter_icon);
+        bindAndSetOrHide(twitter, R.id.tv_contactdetail_twitter, contactDetail.twitter, R.id.iv_twitter_icon);
 
-        BindAndSetOrHide(tango, R.id.tv_contactdetail_tango, contactDetail.tango, R.id.iv_tango_icon);
+        bindAndSetOrHide(tango, R.id.tv_contactdetail_tango, contactDetail.tango, R.id.iv_tango_icon);
 
-        BindAndSetOrHide(hike, R.id.tv_contactdetail_hike, contactDetail.hike, R.id.iv_hike_icon);
+        bindAndSetOrHide(hike, R.id.tv_contactdetail_hike, contactDetail.hike, R.id.iv_hike_icon);
 
-        BindAndSetOrHide(qq, R.id.tv_contactdetail_qq, contactDetail.qq, R.id.iv_qq_icon);
+        bindAndSetOrHide(qq, R.id.tv_contactdetail_qq, contactDetail.qq, R.id.iv_qq_icon);
 
-        BindAndSetOrHide(googletalk, R.id.tv_contactdetail_googletalk, contactDetail.googleTalk, R.id.iv_googletalk_icon);
+        bindAndSetOrHide(googletalk, R.id.tv_contactdetail_googletalk, contactDetail.googleTalk, R.id.iv_googletalk_icon);
 
-        BindAndSetOrHide(viber, R.id.tv_contactdetail_viber, contactDetail.viber, R.id.iv_viber_icon);
+        bindAndSetOrHide(viber, R.id.tv_contactdetail_viber, contactDetail.viber, R.id.iv_viber_icon);
 
-        BindAndSetOrHide(whatsapp, R.id.tv_contactdetail_whatsapp, contactDetail.whatsApp, R.id.iv_whatsapp_icon);
+        bindAndSetOrHide(whatsapp, R.id.tv_contactdetail_whatsapp, contactDetail.whatsApp, R.id.iv_whatsapp_icon);
 
-        BindAndSetOrHide(wechat, R.id.tv_contactdetail_wechat, contactDetail.weChat, R.id.iv_wechat_icon);
+        bindAndSetOrHide(wechat, R.id.tv_contactdetail_wechat, contactDetail.weChat, R.id.iv_wechat_icon);
 
         boolean isValidPhoneNumber = !TextUtils.isEmpty(contactDetail.phone);
 
@@ -105,18 +105,18 @@ public class ContactDetailsBottomSheetFragment extends BottomSheetDialogFragment
         return false;
     }
 
-    private void BindAndSetOrHide(TextView textView, int viewId, String string) {
+    private void bindAndSetOrHide(TextView textView, int viewId, String string) {
         textView = rootView.findViewById(viewId);
-        if (TextUtils.isEmpty(string)) {
+        if (TextUtils.isEmpty(string) || TextUtils.equals("null", string)) {
             textView.setVisibility(View.GONE);
         } else {
             textView.setText(string);
         }
     }
 
-    private void BindAndSetOrHide(TextView textView, int viewId, String string, int iconId) {
+    private void bindAndSetOrHide(TextView textView, int viewId, String string, int iconId) {
         textView = rootView.findViewById(viewId);
-        if (TextUtils.isEmpty(string)) {
+        if (TextUtils.isEmpty(string) || TextUtils.equals("null", string)) {
             textView.setVisibility(View.GONE);
             rootView.findViewById(iconId).setVisibility(View.GONE);
         } else {
